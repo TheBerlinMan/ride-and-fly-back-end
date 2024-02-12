9f32b286773b6b2c3b0f4b5cbfc9328301a1d4e3
Mathew <muckele7@gmail.com>
2024-02-12 15:58:11 -0500

    creates trips.js router and routes

---
+++ controllers/trips.js
@@ -0,0 +1,78 @@
+import { Profile } from "../models/profile.js"
+import { Post } from "../models/post.js"
+
+async function create(req, res) {
+  try {
+    req.body.author = req.user.profile
+    const post = await Post.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { posts: post } },
+      { new: true }
+    )
+    post.author = profile
+    res.status(201).json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+async function index (req,res) {
+  try {
+    const posts = await Post.find({})
+    .populate('author')
+    .sort({createdAt: 'desc'})
+    res.status(200).json(posts)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+async function show(req, res) {
+  try {
+    const post = await Post.findById(req.params.postId)
+    .populate(['author'])
+    res.status(200).json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+async function update(req, res){
+  try {
+    const post = await Post.findByIdAndUpdate(
+      req.params.postId,
+      req.body,
+      { new: true }
+    ).populate('author')
+    res.status(200).json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+async function deletePost(req, res) {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.postId)
+    const profile = await Profile.findById(req.user.profile)
+    profile.post.remove({ _id: req.params.postId })
+    await profile.save()
+    res.json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+export {
+  create,
+  index,
+  show,
+  update,
+  deleteTrip as delete
+}
+
