ec33d5775fea88b6f567146c025c05bc523f8202
Berlin <tommyonik@gmail.com>
2024-02-13 22:07:32 -0500

    update sendMessage model to create a conversation

--- models/conversation.js
+++ models/conversation.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const conversationSchema = new Schema({
-  participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  participants: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
   relatedPost: { type: Schema.Types.ObjectId, ref: 'Post' },
 }, { 
   timestamps: true
