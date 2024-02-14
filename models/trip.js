193983d53cee6c8b942b21b194cf4e886d497117
Mathew <muckele7@gmail.com>
2024-02-14 12:16:22 -0500

    fixes trip

--- models/trip.js
+++ models/trip.js
@@ -6,7 +6,7 @@ const tripSchema = new Schema({
   messages: {type: Schema.Types.ObjectId, ref: 'Messages'},
   carPals: [{type: Schema.Types.ObjectId, ref: 'Profiles'}],
   ratings: {type: Schema.Types.ObjectId, ref: 'Reviews'},
-  posts: {type: Schema.Types.ObjectId, ref: 'Post'},
+  post: {type: Schema.Types.ObjectId, ref: 'Post'},
 
 },{
   timestamps: true,
